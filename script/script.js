2ae9e883e1019c12df05e026da96466909b2b52d
Viacheslav Denysko <deniskosv@gmail.com>
2020-08-06 21:35:31 +0300

    layout modal phone

--- script/script.js
+++ script/script.js
@@ -297,4 +297,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
   accord();
   
+  //ModalPhone
+  const btn = document.querySelector('.header-link__phone'),
+    tel = document.querySelector('.header-tel'),
+    close = document.querySelector('.header-tel__close');
+
+  const coordBtn = btn.getBoundingClientRect(),
+    coordTel = tel.getBoundingClientRect();
+  const showPhone = () => {
+    tel.style.left = coordBtn.x + coordBtn.width - coordTel.width + 'px';
+    tel.style.opacity = '1';
+  };
+
+  btn.addEventListener('click', () => {
+    showPhone();
+  });
+
+  close.addEventListener('click', () => {
+    tel.style.opacity = '0';
+  })
+
+  
 });
