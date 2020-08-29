a954af8e1310ca691f283ca963d6d9f9a2905836
Viacheslav Denysko <deniskosv@gmail.com>
2020-08-29 13:54:33 +0300

    done modal for teachers

--- src/modules/modalTeachers.js
+++ src/modules/modalTeachers.js
@@ -3,11 +3,10 @@ const modalTeachers = () => {
   const popup = document.querySelector('.popup'),
     popupContent = popup.querySelector('.popup-content'),
     teacher = popup.querySelector('.popup-teacher'),
-    subject = popup.querySelector('.popup-subject');
+    subject = popup.querySelector('.popup-subject'),
+    btns = document.querySelectorAll('.teachers-btn');
 
   let flyInterval, count = -40, animateStart = true;
-
-  document.body.style.overflow = 'hidden';
 
   const startAnimate = () => {
     if (document.documentElement.clientWidth < 768) {
@@ -18,8 +17,9 @@ const modalTeachers = () => {
 
     flyInterval = requestAnimationFrame(startAnimate);
     count++;
+
     if (count < 10) {
-      popupContent.style.top = count * 2 + '%';
+      popupContent.style.top = count * 1.5 + '%';
       popup.style.display = 'flex';
     } else {
       count = -40;
@@ -30,7 +30,9 @@ const modalTeachers = () => {
   document.body.addEventListener('click', event => {
     let target = event.target;
 
-    if (target.classList.contains('popup-btn')) {
+    if (target.classList.contains('teachers-btn')) {
+      document.body.style.overflow = 'hidden';
+
       if (!animateStart) {
         cancelAnimationFrame(flyInterval);
         animateStart = false;
@@ -38,15 +40,38 @@ const modalTeachers = () => {
         flyInterval = requestAnimationFrame(startAnimate);
         animateStart = true;
       }
+
+      if (target.classList.contains('yaroslava-btn')) {
+        teacher.textContent = 'Ярославе';
+        subject.textContent = 'математики';
+      } else if (target.classList.contains('alina-btn')) {
+        teacher.textContent = 'Алине';
+        subject.textContent = 'украинского языка или литературы';
+      } else if (target.classList.contains('irina-btn')) {
+        teacher.textContent = 'Ирине';
+        subject.textContent = 'химии';
+      } else if (target.classList.contains('diana-btn')) {
+        teacher.textContent = 'Диане';
+        subject.textContent = 'английского языка';
+      } else if (target.classList.contains('daria-btn')) {
+        teacher.textContent = 'Дарье';
+        subject.textContent = 'биологии';
+      } else if (target.classList.contains('irina-ukr-btn')) {
+        teacher.textContent = 'Ирине';
+        subject.textContent = 'украинского языка';
+      } 
+      
     }
 
     if (target.classList.contains('popup-close')) {
       popup.style.display = 'none';
+      document.body.style.overflow = 'visible';
     } else {
       target = target.closest('.popup-content');
 
       if (!target) {
         popup.style.display = 'none';
+        document.body.style.overflow = 'visible';
       }
     }
   });
